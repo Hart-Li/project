049732a4fffe51b9229e5a3e1fa64cbcb4358a53
hartli <463644535@qq.com>
2025-01-21 00:06:36 +0800

    feat: init

---
+++ eslint.config.js
@@ -0,0 +1,73 @@
+import pluginJs from '@eslint/js'
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
+import pluginVue from 'eslint-plugin-vue'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+import VueEslintParser from 'vue-eslint-parser'
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ['**/*.{js,mjs,cjs,ts,vue}'] },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.es2022,
+        ...globals.node,
+        ...globals.jest,
+        process: true,
+      },
+    },
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...pluginVue.configs['flat/essential'],
+  {
+    plugins: {
+      pluginVue: pluginVue,
+    },
+    rules: {
+      'vue/multi-word-component-names': 'off', // 要求组件名称始终为 "-" 链接的单词
+      'vue/script-setup-uses-vars': 'error', // 防止<script setup>使用的变量<template>被标记为未使用
+      'vue/no-mutating-props': 'off', // 不允许组件 prop的改变
+      'vue/attribute-hyphenation': 'off', // 对模板中的自定义组件强制执行属性命名样式
+    },
+  },
+  {
+    plugins: {
+      tseslint: tseslint,
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'error', // 禁止定义未使用的变量
+      '@typescript-eslint/prefer-ts-expect-error': 'error', // 禁止使用 @ts-ignore
+      '@typescript-eslint/no-explicit-any': 'off', // 禁止使用any类型
+      '@typescript-eslint/no-non-null-assertion': 'off', // 禁止使用非空断言
+      '@typescript-eslint/no-namespace': 'off', // 禁止使用自定义 TypeScript 模块和命名空间。
+      '@typescript-eslint/semi': 'off', // 要求或禁止使用分号
+    },
+  },
+  {
+    rules: {
+      'no-var': 'error', // 要求使用 let 或 const 而不是 var
+      'no-multiple-empty-lines': ['warn', { max: 1 }], // 禁止多行空行
+      'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off', // 禁止使用console
+      'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off', // 禁止使用debugger
+      'no-unexpected-multiline': 'error', // 禁止空余的多行
+      'no-useless-escape': 'off', // 禁止不必要的转义字符
+    },
+  },
+  {
+    languageOptions: {
+      parser: VueEslintParser,
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+      },
+    },
+  },
+  {
+    ignores: ['dist', 'node_modules'],
+  },
+  eslintPluginPrettierRecommended,
+]
