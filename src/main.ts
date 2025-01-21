306524a681a4fbcf5ee142cc8fa1bfde1ddc2f6a
hartli <463644535@qq.com>
2025-01-21 20:48:16 +0800

    feat: 集成 element-plus & 项目别名配置

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,12 @@
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 import { createApp } from 'vue'
 import App from './App.vue'
+//@ts-expect-error Element Plus 的中文语言包类型定义问题
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(ElementPlus, {
+  locale: zhCn,
+})
+app.mount('#app')
