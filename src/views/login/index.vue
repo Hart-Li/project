154764d544eca6fee8dea15ec0b1ad4374d3e013
lijianwei01 <lijianwei01@zhihu.com>
2025-02-05 19:10:15 +0800

    feat: 登录静态页面

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,9 +1,68 @@
 <template>
-  <div>
-    <h1>Login</h1>
+  <div class="login_container">
+    <el-row>
+      <el-col :span="12" :xs="0"></el-col>
+      <el-col :span="12" :xs="24">
+        <el-form class="login_form">
+          <h1>Hello</h1>
+          <h2>欢迎来到后台管理系统</h2>
+          <el-form-item>
+            <el-input
+              v-model="loginForm.username"
+              :prefix-icon="User"
+            ></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-input
+              type="password"
+              v-model="loginForm.password"
+              show-password
+              :prefix-icon="Lock"
+            ></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-button class="login_btn" round>登录</el-button>
+          </el-form-item>
+        </el-form>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { reactive } from 'vue'
+import { User, Lock } from '@element-plus/icons-vue'
 
-<style scoped></style>
+let loginForm = reactive({ username: 'admin', password: '111111' })
+</script>
+
+<style scoped lang="scss">
+.login_container {
+  width: 100%;
+  height: 100vh;
+  background: url('@/assets/images/code.png') no-repeat;
+  background-size: cover;
+  .login_form {
+    position: relative;
+    width: 80%;
+    top: 30vh;
+    padding: 40px;
+    background: rgba(207, 89, 38, 0.5);
+
+    h1 {
+      font-size: 40px;
+      font-weight: 700;
+      color: white;
+    }
+    h2 {
+      font-size: 20px;
+      font-weight: 400;
+      color: white;
+      margin: 20px 0;
+    }
+    .login_btn {
+      width: 100%;
+    }
+  }
+}
+</style>
