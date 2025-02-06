7b49ea1a6d749715d907c536bd477fe4bca06ba9
lijianwei01 <lijianwei01@zhihu.com>
2025-02-06 17:52:03 +0800

    feat: 增加登录表单的规则校验

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -3,16 +3,24 @@
     <el-row>
       <el-col :span="12" :xs="0"></el-col>
       <el-col :span="12" :xs="24">
-        <el-form class="login_form">
+        <el-form
+          class="login_form"
+          ref="loginFormRef"
+          :model="loginForm"
+          :rules="rules"
+          label-width="auto"
+          status-icon
+        >
           <h1>Hello</h1>
           <h2>欢迎来到后台管理系统</h2>
-          <el-form-item>
+          <el-form-item prop="username">
             <el-input
               v-model="loginForm.username"
               :prefix-icon="User"
+              autofocus
             ></el-input>
           </el-form-item>
-          <el-form-item>
+          <el-form-item prop="password">
             <el-input
               type="password"
               v-model="loginForm.password"
@@ -46,34 +54,81 @@ import { useRouter } from 'vue-router'
 
 const userStore = useUserStore()
 const router = useRouter()
+const loginFormRef = ref()
 
 let loginBtnLoading = ref(false)
-let loginForm = reactive({ username: 'admin', password: '111111' })
+let loginForm = reactive({ username: 'admi', password: '111111' })
 function userLogin() {
   loginBtnLoading.value = true
-  userStore
-    .userLogin({
-      username: loginForm.username,
-      password: loginForm.password,
-    })
+  loginFormRef.value
+    .validate()
     .then(() => {
-      loginBtnLoading.value = false
-      router.push({ path: '/' })
-      const timeIntervalStr = getTimeIntervalStr()
-      ElNotification({
-        title: 'HI, ' + timeIntervalStr,
-        message: '欢迎回来',
-        type: 'success',
-      })
+      userStore
+        .userLogin({
+          username: loginForm.username,
+          password: loginForm.password,
+        })
+        .then(() => {
+          loginBtnLoading.value = false
+          router.push({ path: '/' })
+          const timeIntervalStr = getTimeIntervalStr()
+          ElNotification({
+            title: 'HI, ' + timeIntervalStr,
+            message: '欢迎回来',
+            type: 'success',
+          })
+        })
+        .catch((error) => {
+          loginBtnLoading.value = false
+          ElNotification({
+            title: '登录失败',
+            message: error.message,
+            type: 'error',
+          })
+        })
     })
-    .catch((error) => {
+    .catch(() => {
       loginBtnLoading.value = false
       ElNotification({
         title: '登录失败',
-        message: error.message,
+        message: '请检查字段是否合法',
         type: 'error',
       })
     })
+}
+const userValidator = (rule: any, value: any, callback: any) => {
+  // 用户名正则校验规则
+  const usernameReg = /^[a-zA-Z0-9]{5,15}$/
+  if (!usernameReg.test(value)) {
+    callback(new Error('用户名需要5-15位的英文与数字组合'))
+  } else {
+    callback()
+  }
+}
+const passwordValidator = (rule: any, value: any, callback: any) => {
+  // 密码正则校验规则：密码必须由6-20位的英文、数字或特殊字符的组合
+  const passwordReg = /^[a-zA-Z\d`~!@#$%^&*()-=_+,./\[\]{}<>?]{6,20}$/
+  if (!passwordReg.test(value)) {
+    callback(new Error('密码需要6-20位的英文、数字和特殊字符的组合'))
+  } else {
+    callback()
+  }
+}
+const rules = {
+  username: [
+    {
+      required: true,
+      validator: userValidator,
+      trigger: 'change',
+    },
+  ],
+  password: [
+    {
+      required: true,
+      validator: passwordValidator,
+      trigger: 'change',
+    },
+  ],
 }
 </script>
 
