c92b38145ee8c9944fb1b5f5f1cf52c5186def53
hartli <463644535@qq.com>
2025-02-20 09:22:23 +0800

    feat: 完成首页部分

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,11 +1,30 @@
 <template>
   <div>
-    <h1>Home</h1>
+    <el-card>
+      <div class="header-content">
+        <el-avatar
+          :size="150"
+          :src="userStore.avatar"
+          style="margin: 0 20px"
+        ></el-avatar>
+        <div class="user-text">
+          <p class="first-line">
+            {{ getTimeIntervalStr() }}，{{ userStore.username }}
+          </p>
+          <p class="second-line">欢迎来到后台管理系统</p>
+        </div>
+      </div>
+    </el-card>
+    <div class="main-content">
+      <SvgIcon name="welcome" width="500px" height="500px" />
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import SvgIcon from '@/components/SvgIcon/index.vue'
 import useUserStore from '@/store/modules/user'
+import { getTimeIntervalStr } from '@/utils/time'
 import { onMounted } from 'vue'
 
 const userStore = useUserStore()
@@ -14,4 +33,25 @@ onMounted(() => {
 })
 </script>
 
-<style scoped></style>
+<style scoped>
+.header-content {
+  display: flex;
+  align-items: center;
+  .first-line {
+    font-size: 30px;
+    font-weight: bold;
+    margin-bottom: 30px;
+  }
+  .second-line {
+    font-size: 20px;
+    font-style: italic;
+    color: #999;
+  }
+}
+
+.main-content {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
