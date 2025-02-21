4c3f4e16c24a215feef8da4fc6be08d83d5fb23f
lijianwei01 <lijianwei01@zhihu.com>
2025-02-21 17:35:05 +0800

    feat: 开发数据大盘顶部 30%

---
+++ src/views/screen/components/top/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="top">Top区域</div>
+</template>
+
+<script setup lang="ts"></script>
+<script lang="ts">
+export default {
+  name: 'Top',
+}
+</script>
+
+<style scoped lang="scss">
+.top {
+  width: 100%;
+  height: 40px;
+}
+</style>
--- src/views/screen/index.vue
+++ src/views/screen/index.vue
@@ -1,7 +1,47 @@
 <template>
-  <div>数据大屏</div>
+  <div class="container">
+    <div class="screen" ref="screenRef">
+      <Top />
+    </div>
+  </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import Top from '@/views/screen/components/top/index.vue'
+import { onMounted, ref } from 'vue'
 
-<style lang="scss" scoped></style>
+const calcScale = (w: number = 1920, h: number = 1080) => {
+  const scale = Math.min(window.innerWidth / w, window.innerHeight / h)
+  return `scale(${scale})`
+}
+
+let screenRef = ref()
+
+onMounted(() => {
+  screenRef.value.style.transform = `${calcScale()} translate(-50%, -50%)`
+})
+
+window.onresize = () => {
+  screenRef.value.style.transform = `${calcScale()} translate(-50%, -50%)`
+}
+</script>
+
+<style lang="scss" scoped>
+.container {
+  width: 100vw;
+  height: 100vh;
+  background: url('@/views/screen/images/bg.png') no-repeat;
+  background-size: cover;
+
+  .screen {
+    width: 1920px;
+    height: 1080px;
+    position: fixed;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    transform-origin: left top;
+    background-color: red;
+  }
+}
+</style>
